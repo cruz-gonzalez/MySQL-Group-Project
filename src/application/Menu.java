1180085ce69817c015a46bca27340e8b74399a85
unknown <emailio217gmail.com>
2021-04-10 08:31:41 -0700

    started creating mysql connection functionality within dao package, as well as menu system and usage in the application within the Application package

--- src/application/Menu.java
+++ src/application/Menu.java
@@ -1,5 +1,71 @@
 package application;
 
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
 public class Menu {
+	
+	private Scanner scanner = new Scanner(System.in);
+	private List<String> options = Arrays.asList("Display Teams,",
+			 "Display Team",
+			 "Create Team",
+			 "Delete Team",
+			 "Display Players",
+			 "Display Player",
+			 "Create player",
+			 "Delete Player",
+			 "Display Sponsors",
+			 "Display Sponsor");
+	public void start() {
+		String selection = "";
+		 
+		do {
+			
+			printMenu();
+			selection = scanner.nextLine();
+			
+			if(selection.equals("1")) {
+				//displayTeams();
+			}
+			if(selection.equals("2")) {
+				//displayTeam();
+			}
+			if(selection.equals("3")) {
+				//createTeam();
+			}
+			if(selection.equals("4")) {
+				//deleteTeam();
+			}
+			if(selection.equals("5")) {
+				//displayPlayers();
+			}
+			if(selection.equals("6")) {
+				//displayPlayer();
+			}
+			if(selection.equals("7")) {
+				//createPlayer();
+			}
+			if(selection.equals("8")) {
+			//	deletePlayer();
+			}
+			if(selection.equals("9")) {
+				//displaySponsors();
+			}
+			if(selection.equals("10")) {
+				//displaySponsor();
+			}
+			
+			System.out.println("Press 'enter' to continue");
+			scanner.nextLine();
+			
+		} while (!selection.equals("-1"));
+	}
 
+	private void printMenu() {
+		System.out.println("Select an option\n-------------------");
+		for(int i = 0; i<options.size(); i++){
+			System.out.println(i + 1 + ") " + options.get(i));
+		}
+	}
+	
 }
--- src/entity/PlayoffTeams.java
+++ src/entity/PlayoffTeams.java
@@ -8,7 +8,60 @@ public class PlayoffTeams {
 	private int losses;
 	private char conference;
 	
-	public PlayoffTeams () {
-		
+	public PlayoffTeams (int id, int teamRank, String teamName, int wins, int losses, char conference) {
+		this.setId(id);
+		this.setTeamRank(teamRank);
+		this.setTeamName(teamName);
+		this.setWins(wins);
+		this.setLosses(losses);
+		this.setConference(conference);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getTeamRank() {
+		return teamRank;
+	}
+
+	public void setTeamRank(int teamRank) {
+		this.teamRank = teamRank;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+
+	public int getWins() {
+		return wins;
+	}
+
+	public void setWins(int wins) {
+		this.wins = wins;
+	}
+
+	public int getLosses() {
+		return losses;
+	}
+
+	public void setLosses(int losses) {
+		this.losses = losses;
+	}
+
+	public char getConference() {
+		return conference;
+	}
+
+	public void setConference(char conference) {
+		this.conference = conference;
 	}
 }
