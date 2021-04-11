b0079be2b6843e306d96e5987c437e9751abd3c4
Cruz Gonzalez <cruzgonzalez21@gmail.com>
2021-04-10 17:05:25 -0700

    added playersdao and sponsorsdao code

--- src/dao/PlayersDao.java
+++ src/dao/PlayersDao.java
@@ -3,6 +3,7 @@ package dao;
 import entity.Players;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.ResultSet;
@@ -13,6 +14,7 @@ public class PlayersDao {
 
 	private Connection connection;
 	private final String GET_PLAYERS_QUERY = "SELECT * FROM players";
+	private final String GET_PLAYER_BY_ID_QUERY = "SELECT * FROM players WHERE id = ?";
 
 	public PlayersDao() {
 		connection = DBConnection.getConnection();
@@ -24,14 +26,22 @@ public class PlayersDao {
 		List<Players> players = new ArrayList<Players>();
 		
 		while(rs.next()) {
-			players.add(populatePlayers(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDouble(6),rs.getInt(7), rs.getDouble(8)));
+			players.add(populatePlayer(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDouble(6),rs.getInt(7), rs.getDouble(8)));
 		}
 		
 		return players;
 	}
+	//get all players with proper syntax according to our players table parameters
+	private Players getPlayerById(int id) throws SQLException {
+		PreparedStatement ps = connection.prepareStatement(GET_PLAYER_BY_ID_QUERY);
+		ps.setInt(1,id);
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		return populatePlayer(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getDouble(6),rs.getInt(7), rs.getDouble(8));
+	}
 	
 	//get a player with proper syntax according to our players table parameters
-	private Players populatePlayers(int id, int teamId, String playerName, int playerRank, String team, double averagePoints, int gamesPlayed, double minutesPerGame) {
+	private Players populatePlayer(int id, int teamId, String playerName, int playerRank, String team, double averagePoints, int gamesPlayed, double minutesPerGame) {
 		return new Players(id, teamId, playerName, playerRank, team, averagePoints, gamesPlayed, minutesPerGame);
 	}
 }
